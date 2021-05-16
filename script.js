acd800657fd434472dfeb88bb9233c831ce3a9ae
timotiousPrime <timodb031@gmail.com>
2021-05-16 08:17:31 +0200

    Adds colour to the sketch areas pixel with mouseover

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
 // Slider value
 
 const SLIDER = document.getElementById('sizeRangeSlider');
-console.log(`The grid size is ${SLIDER.value}`)
+console.log(`The grid size is ${SLIDER.value}x${SLIDER.value}`);
+
+let sketchPadSize = SLIDER.value * SLIDER.value;
+console.log(`The sketch pad has ${sketchPadSize} pixels`);
 
 SLIDER.oninput = function() {
-    console.log(`The grid has been changed to the size of ${SLIDER.value}`)
+    console.log(`The grid has been changed to the size of ${SLIDER.value}`);
 }
 
+let sketchPixel = document.createElement('div');
+let sketchPadArea = document.querySelector('.sketchPadArea');
+
+function createPixel(){
+    sketchPixel;
+    ;
+}
+
+// Set the number of columns for the sketch pad pixels
+let gridColumns = '';
+function addGridColumns() {
+    for (let i=0; i < SLIDER.value; i++) {
+        gridColumns += ' auto';
+    }
+    return gridColumns;
+}
+
+gridColumns = addGridColumns();
+
+// add pixels per number set by slider
+console.log(`The grid should be ${sketchPadSize} pixels`);
+
+for (let i=1; i <= sketchPadSize; i++) {
+    let newDiv = document.createElement('div');
+    sketchPadArea.appendChild(newDiv);
+    sketchPixel.textContent = `${i}`;
+    newDiv.classList.add('sketchPixel');
+    let idName = 'pixel' + `${i}`
+    newDiv.setAttribute('id', `${idName}`);
+}
+
+
+sketchPadArea.setAttribute('style', `grid-template-columns: ${gridColumns}`);
+//
+
+
+// Adds colour to each pixel that your mouse goes over
+function addcolor(e) {
+    console.log(e.target.id)
+    pixelId = '#' + e.target.id
+    console.log('The ID of the pixel is ' + pixelId)
+    pixel = document.querySelector(pixelId);
+    pixel.setAttribute('style', 'background: blue')
+}
+
+sketchPadArea.addEventListener('mouseover', addcolor)
+//
